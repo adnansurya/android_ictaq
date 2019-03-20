9ca7b2241bb801fcc848ecbd0dac0d97cf0b0598
Muhammad Adnan Surya <muh.adnansuryaazis@gmail.com>
2019-03-21 00:09:06 +0800

    main activity fix

--- app/src/main/java/elarham/tahfizh/ictaq/CertificateFragment.java
+++ app/src/main/java/elarham/tahfizh/ictaq/CertificateFragment.java
@@ -13,7 +13,7 @@ public class CertificateFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_ustadz, container, false);
+        View view = inflater.inflate(R.layout.fragment_certificate, container, false);
         return view;
     }
 }
--- app/src/main/java/elarham/tahfizh/ictaq/MainActivity.java
+++ app/src/main/java/elarham/tahfizh/ictaq/MainActivity.java
@@ -3,17 +3,29 @@ package elarham.tahfizh.ictaq;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity
         implements BottomNavigationView.OnNavigationItemSelectedListener {
 
+    ActionBar actBar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        actBar = getSupportActionBar();
+        actBar.setDisplayOptions(ActionBar.DISPLAY_SHOW_HOME | ActionBar.DISPLAY_SHOW_TITLE | ActionBar.DISPLAY_USE_LOGO);
+
+        actBar.setIcon(R.mipmap.ic_mytahfizh);
+        actBar.setTitle(" MyTahfizh");
 
         // kita set default nya Home Fragment
         loadFragment(new HomeFragment());
@@ -63,4 +75,15 @@ public class MainActivity extends AppCompatActivity
 
         return loadFragment(fragment);
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.actionbar_menu, menu);
+
+        return true;
+    }
+
+//
+
 }
