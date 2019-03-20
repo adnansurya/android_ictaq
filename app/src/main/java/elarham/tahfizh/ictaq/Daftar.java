70464598cec73c6f12d9bed72b74402f1d7916b0
Muhammad Adnan Surya <muh.adnansuryaazis@gmail.com>
2019-03-20 16:41:43 +0800

    bottom view navigation

---
+++ app/src/main/java/elarham/tahfizh/ictaq/Daftar.java
@@ -0,0 +1,13 @@
+package elarham.tahfizh.ictaq;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Daftar extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_daftar);
+    }
+}
