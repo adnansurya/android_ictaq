925ac22191e2d650a95708536b6edb42385fdf24
Muhammad Adnan Surya <muh.adnansuryaazis@gmail.com>
2019-03-27 18:48:14 +0800

    konek ke db_bro

--- app/src/main/java/elarham/tahfizh/ictaq/Daftar.java
+++ app/src/main/java/elarham/tahfizh/ictaq/Daftar.java
@@ -1,7 +1,6 @@
 package elarham.tahfizh.ictaq;
 
 import android.app.ProgressDialog;
-import android.content.Intent;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -15,11 +14,8 @@ import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
-import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
-
-import org.json.JSONObject;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -30,6 +26,7 @@ public class Daftar extends AppCompatActivity {
     String nama, email, username, password, password2;
     Button daftarBtn;
     ActionBar actBar;
+    RequestQueue queue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -63,49 +60,12 @@ public class Daftar extends AppCompatActivity {
                 }else{
                     if(password.equals(password2)){
 
-                        final ProgressDialog progressDialog = new ProgressDialog(Daftar.this);
-                        progressDialog.setMessage(getApplicationContext().getString(R.string.loading));
-                        progressDialog.show();
 
-                        RequestQueue queue = Volley.newRequestQueue(getApplicationContext());
-                        String url ="http://elarham-tahfizh.online/service/my_service.php?password=7ba52b255b999d6f1a7fa433a9cf7df4&aksi=insert&tabel=registrasi";
 
-                        StringRequest strRequest = new StringRequest(Request.Method.POST, url,
-                                new Response.Listener<String>()
-                                {
-                                    @Override
-                                    public void onResponse(String response)
-                                    {
-                                        Toast.makeText(getApplicationContext(), response, Toast.LENGTH_SHORT).show();
-                                        Log.e("Volley Success", response);
-                                        progressDialog.dismiss();
-                                    }
-                                },
-                                new Response.ErrorListener()
-                                {
-                                    @Override
-                                    public void onErrorResponse(VolleyError error)
-                                    {
-                                        Toast.makeText(getApplicationContext(), error.toString(), Toast.LENGTH_SHORT).show();
-                                        Log.e("Volley Error", error.toString());
-                                        progressDialog.dismiss();;
-                                    }
-                                })
-                        {
-                            @Override
-                            protected Map<String, String> getParams()
-                            {
-                                Map<String, String> params = new HashMap<String, String>();
-                                params.put("username", username);
-                                params.put("type",String.valueOf(1));
-                                params.put("nama",nama);
-                                params.put("password","7ba52b255b999d6f1a7fa433a9cf7df4");
-                                params.put("email",email);
-                                return params;
-                            }
-                        };
+                        queue = Volley.newRequestQueue(getApplicationContext());
+                        setData();
+                        //newData();
 
-                        queue.add(strRequest);
 
                     }else{
                         Toast.makeText(Daftar.this, getApplicationContext().getString(R.string.passwordverificationfail), Toast.LENGTH_LONG).show();
@@ -121,4 +81,51 @@ public class Daftar extends AppCompatActivity {
         });
 
     }
+
+    public void setData(){
+
+        final ProgressDialog progressDialog = new ProgressDialog(Daftar.this);
+        progressDialog.setMessage(getApplicationContext().getString(R.string.loading));
+        progressDialog.show();
+
+        String url ="http://elarham-tahfizh.online/service/my_service.php?password=7ba52b255b999d6f1a7fa433a9cf7df4&aksi=insert&tabel=user";
+
+
+        StringRequest strRequest = new StringRequest(Request.Method.POST, url,
+                new Response.Listener<String>()
+                {
+                    @Override
+                    public void onResponse(String response)
+                    {
+                        Toast.makeText(getApplicationContext(), response, Toast.LENGTH_SHORT).show();
+                        Log.e("Volley Success", response);
+                        progressDialog.dismiss();
+                    }
+                },
+                new Response.ErrorListener()
+                {
+                    @Override
+                    public void onErrorResponse(VolleyError error)
+                    {
+                        Toast.makeText(getApplicationContext(), error.toString(), Toast.LENGTH_SHORT).show();
+                        Log.e("Volley Error", error.toString());
+                        progressDialog.dismiss();;
+                    }
+                })
+        {
+            @Override
+            protected Map<String, String> getParams()
+            {
+                Map<String, String> params = new HashMap<String, String>();
+                params.put("field", "username,password,type,nama");
+                params.put("value","'"+ username + "','" + password + "','" + String.valueOf(3) + "','" + nama + "'");
+
+                return params;
+            }
+        };
+
+        queue.add(strRequest);
+    }
+
+
 }
