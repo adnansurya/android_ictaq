67533d255e6c8858a3fe4a8de57d9c2888033d0f
Muhammad Adnan Surya <muh.adnansuryaazis@gmail.com>
2019-04-04 09:14:21 +0800

    tampilkan data profil

--- app/src/main/java/elarham/tahfizh/ictaq/Profile.java
+++ app/src/main/java/elarham/tahfizh/ictaq/Profile.java
@@ -9,6 +9,7 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.android.volley.Request;
@@ -30,7 +31,8 @@ public class Profile extends AppCompatActivity {
 
     ActionBar actBar;
     SharedPreferenceManager sharePrefMan;
-    String username, kode;
+    String profileData, kode;
+    TextView namaTxt, typeTxt, tahunTxt, provinsiTxt, pekerjaanTxt, tgl_lahirTxt, alamatTxt, telpTxt, emailTxt, ayah_ibuTxt;
 
 
 
@@ -47,6 +49,17 @@ public class Profile extends AppCompatActivity {
         actBar.setTitle(getApplicationContext().getString(R.string.profile));
         actBar.setDisplayHomeAsUpEnabled(true);
 
+        namaTxt = findViewById(R.id.namaTxt);
+        typeTxt = findViewById(R.id.typeTxt);
+        tahunTxt = findViewById(R.id.tahunTxt);
+        provinsiTxt = findViewById(R.id.provinsiTxt);
+        pekerjaanTxt = findViewById(R.id.pekerjaanTxt);
+        tgl_lahirTxt = findViewById(R.id.tgl_lahirTxt);
+        alamatTxt = findViewById(R.id.alamatTxt);
+        telpTxt = findViewById(R.id.telpTxt);
+        emailTxt = findViewById(R.id.emailTxt);
+        ayah_ibuTxt = findViewById(R.id.ayah_ibuTxt);
+
         final ProgressDialog progressDialog = new ProgressDialog(this);
         progressDialog.setMessage(getApplicationContext().getString(R.string.loading));
         progressDialog.show();
@@ -54,7 +67,7 @@ public class Profile extends AppCompatActivity {
         RequestQueue queue = Volley.newRequestQueue(getApplicationContext());
 
         String url = getApplicationContext().getString(R.string.urlmain) +
-                "/service/my_service.php?password=7ba52b255b999d6f1a7fa433a9cf7df4&aksi=select&tabel=registrasi";;
+                "/service/my_service.php?password=7ba52b255b999d6f1a7fa433a9cf7df4&aksi=select&tabel=registrasi";
 
         StringRequest strRequest = new StringRequest(Request.Method.POST, url,
                 new Response.Listener<String>()
@@ -67,8 +80,26 @@ public class Profile extends AppCompatActivity {
 
                         try {
 
-                            JSONObject profileData = new JSONObject(response).getJSONArray("data").getJSONObject(0);
-
+                            JSONObject profile = new JSONObject(response).getJSONArray("data").getJSONObject(0);
+                            profileData = profile.toString();
+                            namaTxt.setText(profile.getString("nama"));
+                            if(sharePrefMan.getSpType().equals("1")){
+                                typeTxt.setText(getApplicationContext().getString(R.string.admin));
+                            }else if(sharePrefMan.getSpType().equals("2")){
+                                typeTxt.setText(getApplicationContext().getString(R.string.examiner));
+                            }else if(sharePrefMan.getSpType().equals("3")){
+                                typeTxt.setText(getApplicationContext().getString(R.string.memorizer));
+                            }else{
+                                typeTxt.setText(sharePrefMan.getSpType());
+                            }
+                            tahunTxt.setText(profile.getString("thn_menghafal"));
+                            provinsiTxt.setText(profile.getString("provinsi"));
+                            pekerjaanTxt.setText(profile.getString("pekerjaan"));
+                            tgl_lahirTxt.setText(profile.getString("tgl_lahir"));
+                            alamatTxt.setText(profile.getString("alamat") + ", " + profile.getString("kota"));
+                            telpTxt.setText(profile.getString("telp"));
+                            emailTxt.setText(profile.getString("email"));
+                            ayah_ibuTxt.setText(profile.getString("ayah_ibu"));
 
 
                         } catch (JSONException e) {
@@ -85,7 +116,7 @@ public class Profile extends AppCompatActivity {
                     {
 
                         Log.e("Volley Error", error.toString());
-                        progressDialog.dismiss();;
+                        progressDialog.dismiss();
                     }
                 })
         {
@@ -118,6 +149,7 @@ public class Profile extends AppCompatActivity {
         switch (item.getItemId()){
             case R.id.edit_menu:
                 profil = new Intent(Profile.this, ProfileEdit.class);
+                profil.putExtra("profileData", profileData);
                 startActivity(profil);
                 break;
         }
