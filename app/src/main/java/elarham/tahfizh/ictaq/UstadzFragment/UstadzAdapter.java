8d4b406ddc98f7879ab0ded0d5462026fdc7dcad
Muhammad Adnan Surya <muh.adnansuryaazis@gmail.com>
2019-04-08 21:31:20 +0800

    fix listview ustadz

--- app/src/main/java/elarham/tahfizh/ictaq/UstadzFragment/UstadzAdapter.java
+++ app/src/main/java/elarham/tahfizh/ictaq/UstadzFragment/UstadzAdapter.java
@@ -5,6 +5,7 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import java.util.List;
@@ -24,27 +25,15 @@ public class UstadzAdapter extends RecyclerView.Adapter<UstadzAdapter.ViewHolder
 
     @Override
     public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        View v = LayoutInflater.from(context).inflate(R.layout.single_surah, parent, false);
+        View v = LayoutInflater.from(context).inflate(R.layout.single_ustadz, parent, false);
         return new ViewHolder(v);
     }
 
     @Override
     public void onBindViewHolder(ViewHolder holder, int position) {
-        Surah surah = list.get(position);
+        User user = list.get(position);
 
-        String type = surah.getType();
-
-        if(type.equals("mekah")){
-            holder.typeTxt.setText(context.getString(R.string.makkiyah)+ ",");
-        }else if(type.equals("madinah")){
-            holder.typeTxt.setText(context.getString(R.string.madaniyah) + ",");
-        }
-        holder.surahTxt.setText(surah.getNama());
-        holder.nomorTxt.setText(surah.getNomor());
-        holder.asmaTxt.setText(surah.getAsma());
-        holder.artiTxt.setText(surah.getArti());
-        holder.ayatTxt.setText(surah.getAyat()+ " " + context.getString(R.string.ayat));
-
+        holder.namaTxt.setText(user.getNama());
 
     }
 
@@ -54,38 +43,37 @@ public class UstadzAdapter extends RecyclerView.Adapter<UstadzAdapter.ViewHolder
     }
 
     public class ViewHolder extends RecyclerView.ViewHolder {
-        public TextView surahTxt, nomorTxt, asmaTxt, artiTxt, ayatTxt, typeTxt;
+        public TextView namaTxt;
+        public ImageView itemImageView;
 
         public ViewHolder(View itemView) {
             super(itemView);
 
-            surahTxt = itemView.findViewById(R.id.surahTxt);
-            nomorTxt = itemView.findViewById(R.id.nomorTxt);
-            asmaTxt = itemView.findViewById(R.id.asmaTxt);
-            artiTxt = itemView.findViewById(R.id.artiTxt);
-            ayatTxt = itemView.findViewById(R.id.ayatTxt);
-            typeTxt = itemView.findViewById(R.id.typeTxt);
+            namaTxt = itemView.findViewById(R.id.namaTxt);
+            itemImageView = itemView.findViewById(R.id.itemImageView);
 
-            itemView.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
 
-                    int position = getAdapterPosition();
 
-                    Surah surah = list.get(position);
-
-                    Intent quran = new Intent(context, Quran.class);
-                    quran.putExtra("nama", surah.getNama());
-                    quran.putExtra("nomor", surah.getNomor());
-                    quran.putExtra("asma", surah.getAsma());
-                    quran.putExtra("arti", surah.getArti());
-                    quran.putExtra("ayat", surah.getAyat());
-                    quran.putExtra("type", surah.getType());
-                    quran.putExtra("keterangan", surah.getKeterangan());
-                    quran.putExtra("mode", "surah");
-                    context.startActivity(quran);
-                }
-            });
+//            itemView.setOnClickListener(new View.OnClickListener() {
+//                @Override
+//                public void onClick(View v) {
+//
+//                    int position = getAdapterPosition();
+//
+//                    Surah surah = list.get(position);
+//
+//                    Intent quran = new Intent(context, Quran.class);
+//                    quran.putExtra("nama", surah.getNama());
+//                    quran.putExtra("nomor", surah.getNomor());
+//                    quran.putExtra("asma", surah.getAsma());
+//                    quran.putExtra("arti", surah.getArti());
+//                    quran.putExtra("ayat", surah.getAyat());
+//                    quran.putExtra("type", surah.getType());
+//                    quran.putExtra("keterangan", surah.getKeterangan());
+//                    quran.putExtra("mode", "surah");
+//                    context.startActivity(quran);
+//                }
+//            });
         }
     }
 
